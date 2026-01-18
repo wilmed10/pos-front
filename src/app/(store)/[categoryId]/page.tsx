5f850d59d988d1c92372b743b29b898fd901f0c1
Wilson Medina <wilsonmedina8d@gmail.com>
2026-01-18 17:35:58 -0500

    feat: make the request to api sending the category id as param, and install zod for validations and set its schemas

--- src/app/(store)/[categoryId]/page.tsx
+++ src/app/(store)/[categoryId]/page.tsx
@@ -1,13 +1,20 @@
+import { CategoryWithProductsResponseSchema } from "@/schemas/schemas"
+
 type Params = Promise<{categoryId: string}>
 
 async function getProducts(categoryId: string) {
-    console.log(categoryId)
+    const url = `${process.env.API_URL}/categories/${categoryId}?products=true`
+    const req = await fetch(url)
+    const json = await req.json()
+    const products =CategoryWithProductsResponseSchema.parse(json)
+    return products
 }
 
 export default async function StorePage({params}: {params: Params}) {
 
     const { categoryId } = await params
-    await getProducts(categoryId)
+    const products = await getProducts(categoryId)
+    console.log(products)
 
     return (
         <>
