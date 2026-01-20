a5bd127d8ccd33c22301001094a090ddb15b7850
Wilson Medina <wilsonmedina8d@gmail.com>
2026-01-20 13:58:36 -0500

    feat: map items added to cart and allow update quantity of each one

--- src/components/cart/ShoppingCart.tsx
+++ src/components/cart/ShoppingCart.tsx
@@ -1,8 +1,26 @@
+'use client'
+
+import { useStore } from "@/store/store"
+import ShoppingCartItem from "./ShoppingCartItem"
 
 export default function ShoppingCart() {
-  return (
-    <>
-        <h2 className="text-4xl font-bold text-gray-900">Resumen de Venta</h2>
-    </>
-  )
+
+    const contents = useStore(state => state.contents)
+
+    return (
+        <>
+            <h2 className="text-4xl font-bold text-gray-900">Resumen de Venta</h2>
+
+            <ul role="list" className="mt-6 divide-y divide-gray-200 border-t border-gray-200 text-sm fonr-medium text-gray-500">
+
+                {contents.map(item => (
+                    <ShoppingCartItem
+                        key={item.productId}
+                        item={item}
+                    />
+                ))}
+
+            </ul>
+        </>
+    )
 }
