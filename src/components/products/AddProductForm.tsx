db4983aae6ed971cfeed7182792a9efe5326c968
Wilson Medina <wilsonmedina8d@gmail.com>
2026-01-28 21:28:38 -0500

    feat: implement addProduct action and ProductFormSchema for product validation

--- src/components/products/AddProductForm.tsx
+++ src/components/products/AddProductForm.tsx
@@ -1,16 +1,40 @@
 'use client'
 
+import { addProduct } from "@/actions/add-product-action"
+import { useRouter } from "next/router"
+import { useActionState, useEffect } from "react"
+import { toast } from "react-toastify"
+
 export default function AddProductForm({children}: {children: React.ReactNode}) {
-  return (
-    <form
-        className="space-y-5"
-    >
-        {children}
-        <input
-            type="submit"
-            className="rounded bg-green-400 font-bold py-2 w-full cursor-pointer"
-            value="Agregar producto"
-        />
-    </form>
-  )
+    
+    const router = useRouter()
+    
+    const [state, dispatch] = useActionState(addProduct, {
+        errors: [],
+        success: ''
+    })
+
+    useEffect(() => {
+        if(state.errors) {
+            state.errors.forEach(error => toast.error(error))
+        }
+        if(state.success) {
+            toast.success(state.success)
+            router.push('/admin/products')
+        }
+    }, [state])
+
+    return (
+        <form
+            className="space-y-5"
+            action={dispatch}
+        >
+            {children}
+            <input
+                type="submit"
+                className="rounded bg-green-400 font-bold py-2 w-full cursor-pointer"
+                value="Agregar producto"
+            />
+        </form>
+    )
 }
--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -87,6 +87,16 @@ export const TransactionResponseSchema = z.object({
   
 export const TransactionsResponseSchema = z.array(TransactionResponseSchema)
 
+export const ProductFormSchema = z.object({
+  name: z.string()
+          .min(1, {message: 'El Nombre del Producto no puede ir vacio'}),
+  price: z.coerce.number({message: 'Precio no válido'})
+          .min(1, {message: 'El Precio debe ser mayor a 0'}),
+  inventory: z.coerce.number({message: 'Inventario no válido'})
+          .min(1, {message: 'El inventario debe ser mayor a 0'}),
+  categoryId: z.coerce.number({message: 'La Categoria no es válida'})
+})
+
 export type ShoppingCart = z.infer<typeof ShoppingCartSchema>
 export type CartItem = z.infer<typeof ShoppingCartContentsSchema>
 export type Coupon = z.infer<typeof CouponResponseSchema>
